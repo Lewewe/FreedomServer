fc4f13c5e5984395b355346c8c7980da06d00176
Lewewe <63868370+Lewewe@users.noreply.github.com>
2023-08-12 17:46:47 +0900

    Increase the default timeout to 3 seconds and add a command line
    switch to control that

--- source/app.d
+++ source/app.d
@@ -40,6 +40,8 @@ enum dsId = -2;
 __gshared ADI v1Adi;
 __gshared Device v1Device;
 
+__gshared Duration timeout;
+
 int main(string[] args) {
 	debug {
 		configureLoggingProvider(new shared DefaultProvider(true, Levels.DEBUG));
@@ -57,14 +59,19 @@ int main(string[] args) {
 	string certificateChainPath = null;
 	string privateKeyPath = null;
 
+	long timeoutMsecs = 3000;
+
 	auto helpInformation = getopt(
 		args,
 		"n|host", format!"The hostname to bind to (default: %s)"(hostname), &hostname,
 		"p|port", format!"The port to bind to (default: %s)"(port), &port,
 		"a|adi-path", format!"Where the provisioning information should be stored on the computer for anisette-v1 backwards compat (default: %s)"(configurationPath), &configurationPath,
+		"timeout", format!"Timeout duration for Anisette V3 in milliseconds (default: %d)"(timeoutMsecs), &timeoutMsecs,
 		"private-key", "Path to the PEM-formatted private key file for HTTPS support (requires --cert-chain)", &certificateChainPath,
 		"cert-chain", "Path to the PEM-formatted certificate chain file for HTTPS support (requires --private-key)", &privateKeyPath,
 	);
+
+	timeout = dur!"msecs"(timeoutMsecs);
 
 	if ((certificateChainPath && !privateKeyPath) || (!certificateChainPath && privateKeyPath)) {
 		log.error("--certificate-chain and --private-key must both be specified for HTTPS support (they can be both be in the same file though).");
@@ -241,7 +248,10 @@ class AnisetteService {
 			file.write(provisioningPath.buildPath("adi.pb"), adi_pb);
 
 			GC.disable(); // garbage collector can deallocate ADI parts since it can't find the pointers.
-			scope(exit) GC.enable();
+			scope(exit) {
+				GC.enable();
+				GC.collect();
+			}
 
 			ADI adi = makeGarbageCollectedADI(libraryPath);
 			adi.provisioningPath = provisioningPath;
@@ -281,8 +291,6 @@ class AnisetteService {
 			}
 		}
 	}
-
-	enum timeout = dur!"msecs"(1250);
 
 	@method(HTTPMethod.GET)
 	@path("/v3/provisioning_session")
@@ -330,7 +338,10 @@ class AnisetteService {
 		log.infoF!("[<< %s] Correct identifier (%s).")(requestUUID, identifier);
 
 		GC.disable(); // garbage collector can deallocate ADI parts since it can't find the pointers.
-		scope(exit) GC.enable();
+		scope(exit) {
+			GC.enable();
+			GC.collect();
+		}
 		ADI adi = makeGarbageCollectedADI(libraryPath);
 		auto provisioningPath = file.getcwd()
 			.buildPath("provisioning")
@@ -431,7 +442,7 @@ class AnisetteService {
 
 private ADI makeGarbageCollectedADI(string libraryPath) {
 	extern(C) void* malloc_GC(size_t sz) {
-		return GC.malloc(sz);
+		return GC.malloc(sz, GC.BlkAttr.NO_MOVE);
 	}
 
 	extern(C) void free_GC(void* ptr) {
